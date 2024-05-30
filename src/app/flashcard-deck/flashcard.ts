a8c445ef85c21a526a03f107d1d7cef630479edd
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-05-30 17:01:52 +0300

    Add docs for the model

--- src/app/flashcard-deck/flashcard.ts
+++ src/app/flashcard-deck/flashcard.ts
@@ -1,6 +1,24 @@
+/**
+ * Represents a flashcard.
+ */
 export interface Flashcard {
+    /**
+     * The question on the flashcard.
+     */
     question: string;
+    
+    /**
+     * The options for the flashcard.
+     */
     options: string[];
+    
+    /**
+     * The index of the correct answer in the options array.
+     */
     correctAnswerIndex: number;
+    
+    /**
+     * The number of points assigned to the flashcard.
+     */
     points: number;
 }
